6a49d1b60dcc6f5944b712964a37114001ca3ddd
ialsaud <i.m.s@msn.com>
2016-09-18 18:39:03 -0400

    First commit
    Latest version of Site Style Tree made made by Salman and Ibrahim

---
+++ test.py
@@ -0,0 +1,15 @@
+from SiteStyleTree import *
+
+
+
+PST = page_tree('http://www.securityfocus.com/bid/83265')
+print print_PST(PST.root)
+
+
+#SST = StyleTree()
+#SST.add_page(PST)
+
+
+##print SST.children[0].occur
+
+
